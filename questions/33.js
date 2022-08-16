dcbb15aa0201bb31cbefc4e75ad4bb3d27fe94cf
Stefania Popa <stefania_florina10@yahoo.com>
2022-08-16 18:28:40 +0300

    did more testing

---
+++ questions/33.js
@@ -0,0 +1,8 @@
+//33.Two sum problem  
+/** Problem
+Given an integer x and a sorted array a of N distinct integers, design a linear-time algorithm to determine if there exists two distinct indices i and j such that a[i] + a[j] == x
+
+For example, if the array is [3, 5, 2, -4, 8, 11] and the sum is 7, your program should return [[11, -4], [2, 5]] because 11 + -4 = 7 and 2 + 5 = 7.
+Answer
+The algorithm below makes use of hash tables which have a constant lookup time. As we pass through each element in the array, we check to see if S minus the current element exists in the hash table. We only need to loop through the array once, resulting in a running time of O(n) since each lookup and insertion in a hash table is O(1). */
+
